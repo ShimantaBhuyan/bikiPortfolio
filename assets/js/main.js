32cf8b6e9034bd903ab8037f2250c178887259a3
Shimanta Bhuyan <bhuyanshimanta@gmail.com>
2020-04-25 18:55:02 +0530

    Added new page and related

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
         "edge" : "right",
     }
     var instance1 = M.Sidenav.init(sidenavElem, sidenavOptions);
+
+    var modalElem = document.querySelectorAll('.modal');
+    var modalOptions = {
+        "opacity:": "0.2",
+        "startingTop": "92px",
+    };
+    var instance2 = M.Modal.init(modalElem, modalOptions);
 });
