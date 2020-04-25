ae5b25a633a6f285edeeb3914226f4f929a03334
Shimanta Bhuyan <bhuyanshimanta@gmail.com>
2020-04-25 23:15:41 +0530

    used modaloptions to show and hide closemodal

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     var sidenavOptions = {
         "edge" : "right",
     }
-    var instance1 = M.Sidenav.init(sidenavElem, sidenavOptions);
+    var instanceSidenav = M.Sidenav.init(sidenavElem, sidenavOptions);
 
     var modalElem = document.querySelectorAll('.modal');
-    var modalOptions = {};
-    var instance2 = M.Modal.init(modalElem, modalOptions);
+    var modalOptions = {
+        onOpenEnd: () => {
+            document.getElementById('modalCloseBtn').style.display = 'block';
+        },
+        onCloseStart: () => {
+            document.getElementById('modalCloseBtn').style.display = 'none';
+        }
+    }
+    var instanceModal = M.Modal.init(modalElem, modalOptions);
 });
- 
-function checkModalClose(close) {
-    if (close == 'off') {
-        document.getElementById('modalCloseBtn').style.display = 'none';
-    }
-    else if(close == 'on')  {
-        document.getElementById('modalCloseBtn').style.display = 'block';  
-    }
-}
