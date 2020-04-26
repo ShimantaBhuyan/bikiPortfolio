569502b127bcd9700d73e2184d6219bd201cebf5
ShimantaBhuyan <bhuyanshimanta@gmail.com>
2020-04-27 00:34:07 +0530

    revisions v1.0 + responsive for 360px screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
+var modalOptions = {
+    "opacity": "0.9",
+    onOpenEnd: () => {
+        document.getElementById('modalCloseBtn').style.display = 'block';
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     var sidenavElem = document.querySelectorAll('.sidenav');    
     var sidenavOptions = {
         "edge" : "right",
     }
-    var instanceSidenav = M.Sidenav.init(sidenavElem, sidenavOptions);
+    var sidenavInstance = M.Sidenav.init(sidenavElem, sidenavOptions);
 
     var modalElem = document.querySelectorAll('.modal');
-    var modalOptions = {
-        onOpenEnd: () => {
-            document.getElementById('modalCloseBtn').style.display = 'block';
-        },
-        onCloseStart: () => {
-            document.getElementById('modalCloseBtn').style.display = 'none';
-        }
-    }
-    var instanceModal = M.Modal.init(modalElem, modalOptions);
+    var modalInstance = M.Modal.init(modalElem, modalOptions);
 });
+
+function closeModal(){
+    var modalElem = document.querySelectorAll('.modal');
+    var instanceOfModal = M.Modal.init(modalElem, modalOptions)[0];
+    instanceOfModal.close();
+    document.getElementById('modalCloseBtn').style.display = 'none';
+    document.getElementsByTagName("body")[0].style.overflow = "";
+}
